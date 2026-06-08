c59db10e61b7e828f728a7df89123b2adf478af1
Dan Nisenson <dannisensonc@gmail.com>
2026-06-08 16:34:15 +0200

    feat: add echo server

--- lib/kv.ex
+++ lib/kv.ex
@@ -6,7 +6,9 @@ defmodule KV do
   def start(_mode, _opts) do
     children = [
       {Registry, name: KV, keys: :unique},
-      {DynamicSupervisor, name: KV.BucketSupervisor, strategy: :one_for_one}
+      {DynamicSupervisor, name: KV.BucketSupervisor, strategy: :one_for_one},
+      {Task.Supervisor, name: KV.ServerSupervisor},
+      Supervisor.child_spec({Task, fn -> KV.Server.accept(4040) end}, restart: :permanent)
     ]
 
     Supervisor.start_link(children, strategy: :one_for_one)
